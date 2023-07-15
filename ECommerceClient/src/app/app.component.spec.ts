68adeaada745619723b8355e8cd7a60008547a99
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2023-07-15 23:25:58 +0300

    15_07_2023_2

--- ECommerceClient/src/app/app.component.spec.ts
+++ ECommerceClient/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularECommerceClient'`, () => {
+  it(`should have as title 'ECommerceClient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularECommerceClient');
+    expect(app.title).toEqual('ECommerceClient');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularECommerceClient app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ECommerceClient app is running!');
   });
 });
---
+++ ECommerceServer/Core/ECommerceServer.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceServer.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceServer.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
